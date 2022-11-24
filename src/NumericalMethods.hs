089168a1dfc03fab40e083e215896493a28c3bc3
Christian Fleury <chfleurysiq@gmail.com>
2022-11-24 10:23:12 -0300

    fix: functions

--- src/NumericalMethods.hs
+++ src/NumericalMethods.hs
@@ -3,18 +3,17 @@ module NumericalMethods
     compositeTrapeizoidal
     ) where
 
-import Language.Haskell.Interpreter
- 
 e = exp 1
 
 fun :: Double -> Double
-fun x = eval "(x**2)   * (e**(-x))" -- x^2e^-x dx
+fun x = (x**2)   * (e**(-x))-- x^2e^-x dx
 
 trapezoidal :: Double -> Double -> Double
 trapezoidal a b = ((fun a) + (fun b)) * ((b-a)/2)
 
+h :: Double -> Double -> Double ->  Double
 h a b n = (b-a)/n
 
 compositeTrapeizoidal :: Double -> Double -> Double -> Double
-compositeTrapeizoidal __ 0 = 0
+compositeTrapeizoidal _ _ 0 = 0
 compositeTrapeizoidal a b n = ((fun a) + (fun b) + 2 * (sum [fun (a + (i* h a b n)) | i <- [1.. ((b-a)/h a b n)]])) * ((h a b n) / 2)

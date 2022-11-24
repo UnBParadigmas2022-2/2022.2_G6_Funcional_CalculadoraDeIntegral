54d69e78fa276cb8da27f3e135ef29b918800fad
Christian Fleury <chfleurysiq@gmail.com>
2022-11-24 13:44:20 -0300

    feat: fix eulers number

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,27 @@
+ {-# LANGUAGE OverloadedStrings #-}
 
 module Main (main) where
 
 import NumericalMethods
-import Eval
+import Language.Haskell.Interpreter
+import Data.Text(pack, unpack, replace)
+
+replaceEuler :: String -> String
+replaceEuler = unpack . replace "e" "(exp 1)" . pack
 
 main :: IO ()
 main = do
-    let fExpr = "let f x = x**2 in f"
-    evalExpr fExpr
+    let fExpr =   "let f x = " ++ replaceEuler "e * x**2 in f"
+
+    r <- runInterpreter $ do
+            setImports ["Prelude"]
+            interpret fExpr (as :: Double -> Double)
+    -- run it and get an interface to the function
+    case r of
+        Left err -> putStrLn $ "Ups... " ++ (show err)
+        Right f  -> do
+            print $ f 10.0 
+            putStr (show (compositeTrapeizoidal 1 7 10000 f))
     -- case r of
     --     Left err -> putStrLn $ "Ups... " ++ (show err)
     --     Right f  -> do

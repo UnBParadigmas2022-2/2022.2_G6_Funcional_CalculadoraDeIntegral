effc795d56e801f9df05294cde6a7706ededa108
Christian Fleury <chfleurysiq@gmail.com>
2022-11-24 19:29:54 -0300

    feat: start interacting with user input

--- src/Main.hs
+++ src/Main.hs
@@ -5,12 +5,29 @@ module Main (main) where
 import NumericalMethods
 import Language.Haskell.Interpreter
 import Data.Text(pack, unpack, replace)
+import System.Exit
 
 replaceEuler :: String -> String
 replaceEuler = unpack . replace "e" "(exp 1)" . pack
 
 main :: IO ()
 main = do
+    putStrLn "CALCULADORA DE INTEGRAL"
+    putStrLn "Escolha um método para calcular sua integral:"
+    putStrLn "0    <- Método do trapézio"
+    putStrLn "1    <- Método do trapézio composto"
+    putStrLn "2    <- Sair"
+
+    c <- getChar
+    case c of
+        '0' -> return ()
+        '1' -> putChar '\n' >> putStrLn "Hello World"
+        '2' -> putChar '\n' >> putStrLn "Saindo..." >> exitSuccess
+
+    putStrLn "-------------------"
+
+
+
     let fExpr =   "let f x = " ++ replaceEuler "e * x**2 in f"
 
     r <- runInterpreter $ do
@@ -20,4 +37,8 @@ main = do
     case r of
         Left err -> putStrLn $ "Erro no parse... " ++ (show err)
         Right f  -> do
-            putStr (show (compositeTrapeizoidal 1 7 10000 f))
+            putStr "Resultado: "
+            putStr  (show (compositeTrapeizoidal 1 7 10000 f)) >> putChar '\n'
+            putStrLn "-------------------"
+            
+    main

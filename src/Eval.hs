6cbf931ddce723d74f66a520e3a486c8723b102b
Christian Fleury <chfleurysiq@gmail.com>
2022-11-24 14:27:16 -0300

    chore: comment module

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,20 +1,17 @@
-module Eval
-    ( evalExpr
-    ) where
+-- module Eval
+--     ( evalExpr
+--     ) where
 
-import Language.Haskell.Interpreter
+-- import Language.Haskell.Interpreter
 
--- evalExpr :: (MonadIO m, exceptions-0.10.4:Control.Monad.Catch.MonadMask m) => String -> m (Either InterpreterError (Double -> Double))
-evalExpr fExpr = do
-    -- fExpr is a Haskell code supplied by your user as a String
-    -- Create an interpreter that runs fExpr
-    r <- runInterpreter $ do
-            setImports ["Prelude"]
-            interpret fExpr (as :: Double -> Double)
-    -- run it and get an interface to the function
-    case r of
-        Left err -> putStrLn $ "Ups... " ++ (show err)
-        Right f  -> do
-            print $ f 10.0 
+-- -- evalExpr :: (MonadIO m, exceptions-0.10.4:Control.Monad.Catch.MonadMask m) => String -> m (Either InterpreterError (Double -> Double))
+-- evalExpr fExpr = do
+--     r <- runInterpreter $ do
+--             setImports ["Prelude"]
+--             interpret fExpr (as :: Double -> Double)
+--     case r of
+--         Left err -> putStrLn $ "Ups... " ++ (show err)
+--         Right f  -> do
+--             print $ f 10.0 
     
    
